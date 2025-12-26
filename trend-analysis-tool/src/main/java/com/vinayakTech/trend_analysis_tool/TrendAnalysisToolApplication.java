e537235853a1dbfacb59339e893f82d19f442e47
Vinayak1812 <110918743+Vinayak1812@users.noreply.github.com>
2025-12-26 23:38:31 +0530

    second commit

---
+++ trend-analysis-tool/src/main/java/com/vinayakTech/trend_analysis_tool/TrendAnalysisToolApplication.java
@@ -0,0 +1,15 @@
+package com.vinayakTech.trend_analysis_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendAnalysisToolApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello,start");
+		SpringApplication.run(TrendAnalysisToolApplication.class, args);
+		System.out.println("hello,end");
+	}
+
+}
---
+++ trend-analysis-tool/src/test/java/com/vinayakTech/trend_analysis_tool/TrendAnalysisToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinayakTech.trend_analysis_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendAnalysisToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
